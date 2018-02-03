19e0634fae1cd1168d8e4cdc0272290515143d4e
A. Laszlo Lazuer <visualjazz@gmail.com>
2018-02-03 03:46:39 -0600

    JSX - Updates the JSX to utilize an es6 class and render multiple elements to an ID.

--- react/react_GS/script.js
+++ react/react_GS/script.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <h1>hello world!!!</h1>,
-  document.getElementById('firstapp')
-);
+
+class App extends React.Component {
+  	render() {
+  	return (
+    	<div>
+      <h1>Hello</h1>
+      <button>One</button>
+      <button>One</button>
+      </div>
+    );
+  }
+}
+
+
+// ReactDOM.render(
+//   <h1>hello world!</h1>,
+//   document.getElementById('firstapp')
+// );
+
+
+ReactDOM.render(<App />, document.getElementById('firstapp'));
+
+// ReactDOM.render(
+//   <button>Go</button>,
+//   document.getElementById('firstElement')
+// );
